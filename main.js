b7fa3da32a7118be6a502315a109598a1ec44838
UmutBey1903 <uttasdemir16@gmail.com>
2023-07-24 15:52:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuBtn=document.querySelector('.navbar-menu-btn');
+
+const menuLinks=document.querySelector('.navbar-links');
+
+menuBtn.addEventListener('click', ()=>{
+    menuLinks.classList.toggle('mobile-menu');
+})
